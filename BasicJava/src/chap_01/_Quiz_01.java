aeba2a208845629d1d39bc6727de03c5335bda29
Junbro0708 <aktm708@naver.com>
2023-07-06 17:47:21 +0900

    initialized

---
+++ BasicJava/src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busName = "관악01";
+        int leftTime = 23;
+        double leftDistance = 1.2;
+
+        System.out.println(busName + "번 버스");
+        System.out.println("약" + leftTime + "분 후 도착 예정");
+        System.out.println("남은 거리 " + leftDistance + "km");
+    }
+}
